80b69688ad5bc25ec69481895bce73ce2dcd8f02
Shadi <92844176+ShadiZargar@users.noreply.github.com>
2024-11-26 15:41:42 +0330

    local host + login

--- jwt_token_authentication/settings.py
+++ jwt_token_authentication/settings.py
@@ -109,7 +109,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMidleware',
+    # 'corsheaders.middleware.CorsMidleware',
+    'corsheaders.middleware.CorsMiddleware',
+
 
 
 
