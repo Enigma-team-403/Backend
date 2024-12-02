749c33d21db3493180c4264ab11b7b946b8f0cfe
Shadi <92844176+ShadiZargar@users.noreply.github.com>
2024-12-02 13:47:06 +0330

    Removed all files from branch fixedplnner1

--- members/signals.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.signals import user_logged_in
-from django.dispatch import receiver
-from django.utils import timezone
-from .models import User
-
-@receiver(user_logged_in)
-def update_last_active(sender, request, user, **kwargs):
-    user.last_active = timezone.now()
-    user.save()
