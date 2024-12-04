720cca4e8d4074b5306c20091d2a0932b9719980
Zeinab <Znasiri138@gmail.com>
2024-12-03 22:10:32 -0800

    Todo & Profile

---
+++ planner/todo/models.py
@@ -0,0 +1,63 @@
+from django.db import models
+from django.utils import timezone
+
+class List(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
+    create_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+    color = models.CharField(max_length=7)  # Hex color code
+
+    def __str__(self):  
+        return self.name
+
+
+class Task(models.Model):
+    list = models.ForeignKey(List, related_name='tasks', on_delete=models.CASCADE)
+    id = models.BigAutoField(primary_key=True)
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
+    Note= models.CharField(max_length=400)
+    completed = models.BooleanField(default=False)
+    priority = models.IntegerField(default=0)
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE, related_name='tasks') 
+    attachment = models.FileField(upload_to='attachments/', null=True, blank=True)   
+    create_time = models.DateField(auto_now_add=True)
+    start_task = models.DateField(default=timezone.now)
+    end_task = models.DateField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
+
+
+class SubTask(models.Model):
+    task = models.ForeignKey(Task, related_name='subtasks', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
+    completed = models.BooleanField(default=False)
+    priority = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model): 
+    task = models.ForeignKey(Task, related_name='comments', on_delete=models.CASCADE) 
+    text = models.TextField() 
+    created_at = models.DateTimeField(auto_now_add=True) 
+    def __str__(self): 
+        return f"Comment on {self.task.title} at {self.created_at}"
+    
+
+class TaskTag(models.Model):
+    task = models.ForeignKey(Task, related_name='task_tags', on_delete=models.CASCADE)
+    tag = models.ForeignKey(Tag, related_name='task_tags', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.task.title} - {self.tag.name}"
---
+++ planner/todo/views.py
@@ -0,0 +1,106 @@
+from rest_framework import status, viewsets
+from .models import Task, SubTask, Comment ,List ,Tag, TaskTag
+from .serializers import TaskSerializer, SubTaskSerializer, CommentSerializer ,ListSerializer ,TaskTagSerializer, TagSerializer
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from django.utils.dateparse import parse_date
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+    @action(detail=False, methods=['get'])
+    def filter_by_tag(self, request):
+        tag_name = request.query_params.get('tag', None)
+        if tag_name:
+            tasks = self.queryset.filter(tag__name=tag_name)
+        else:
+            tasks = self.queryset.none()
+
+        serializer = self.get_serializer(tasks, many=True)
+        return Response(serializer.data)
+
+    @action(detail=False, methods=['get'])
+    def filter_by_month(self, request):
+        date_str = request.query_params.get('month', None)
+        if date_str:
+            date = parse_date(date_str)
+            if date:
+                tasks = self.queryset.filter(create_time__year=date.year, create_time__month=date.month)
+            else:
+                tasks = self.queryset.none()
+        else:
+            tasks = self.queryset.none()
+
+        serializer = self.get_serializer(tasks, many=True)
+        return Response(serializer.data)
+
+
+    @action(detail=False, methods=['get'])
+    def search(self, request):
+        query = request.query_params.get('task', None)
+        if query:
+            tasks = self.queryset.filter(title__icontains=query)
+            results = [{'task': task.title, 'list': task.list.title} for task in tasks]
+        else:
+            results = []
+
+        return Response(results)
+
+
+
+class SubTaskViewSet(viewsets.ModelViewSet):
+    queryset = SubTask.objects.all()
+    serializer_class = SubTaskSerializer
+
+    @action(detail=False, methods=['get'])
+    def search(self, request):
+        query = request.query_params.get('subtask', None)
+        if query:
+            tasks = self.queryset.filter(title__icontains=query)
+            results = [{'task': task.title, 'list': task.list.title} for task in tasks]
+        else:
+            results = []
+
+        return Response(results)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+    @action(detail=True, methods=['post'])
+    def add_comment(self, request, pk=None):
+        try:
+            task = Task.objects.get(pk=pk)
+        except Task.DoesNotExist:
+            return Response({'error': 'Task not found'}, status=status.HTTP_404_NOT_FOUND)
+
+        comment_data = request.data
+        comment_data['task'] = task.id
+        comment_serializer = CommentSerializer(data=comment_data)
+        if comment_serializer.is_valid():
+            comment_serializer.save()
+            return Response(comment_serializer.data, status=status.HTTP_201_CREATED)
+        return Response(comment_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class TagViewSet(viewsets.ModelViewSet):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+
+    @action(detail=False, methods=['get'], url_path='list-tags')    
+    def list_tags(self, request):
+        tags = self.queryset.all()
+        serializer = self.get_serializer(tags, many=True)
+        return Response(serializer.data)
+
+
+class TaskTagViewSet(viewsets.ModelViewSet):
+    queryset = TaskTag.objects.all()
+    serializer_class = TaskTagSerializer
+
+class ListViewSet(viewsets.ModelViewSet):
+    queryset = List.objects.all()
+    serializer_class = ListSerializer
+
