f9efc6063bcb501a533add87dbeb1f0b1c002038
Shadi <92844176+ShadiZargar@users.noreply.github.com>
2025-01-27 09:16:03 +0330

    user-add

--- profiles/models.py
+++ profiles/models.py
@@ -45,7 +45,11 @@ from django.contrib.auth.models import User
 from django.conf import settings
 
 class Profile(models.Model):
+    # user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    username = models.CharField(max_length=150, unique=True, null=False, blank=False)
+
+    
     first_name = models.CharField(max_length=30, blank=True, null=True)
     last_name = models.CharField(max_length=30, blank=True, null=True)
     email = models.EmailField(max_length=100, blank=True, null=True)
