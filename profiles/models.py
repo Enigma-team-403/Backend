0135966cdafab247efab4430376b6244ce496e69
Shadi <92844176+ShadiZargar@users.noreply.github.com>
2025-01-27 11:38:56 +0330

    access ok

--- profiles/models.py
+++ profiles/models.py
@@ -47,7 +47,7 @@ from django.conf import settings
 class Profile(models.Model):
     # user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    username = models.CharField(max_length=150, unique=True, null=False, blank=False)
+    # username = models.CharField(max_length=150, unique=True, null=False, blank=False)
 
     
     first_name = models.CharField(max_length=30, blank=True, null=True)
